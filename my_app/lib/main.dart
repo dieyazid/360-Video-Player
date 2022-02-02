65dad082316ff3a58c1e2d1ddeb6a1fdddf8fb25
medbenzekri <medbenzekri219@gmail.com>
2022-02-02 11:45:13 +0100

    pckage changed to motion sensors

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,129 +1,274 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+/// An example of using the plugin, controlling lifecycle and playback of the
+/// video.
+
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:video_360/video_360.dart';
-import 'package:intl/intl.dart';
+import 'package:video_player_extra/video_player_extra.dart';
+import 'package:motion_sensors/motion_sensors.dart';
+import 'package:vector_math/vector_math_64.dart' hide Colors;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(home: MyApp()));
+  runApp(
+    MaterialApp(
+      home: _App(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class _App extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: const ValueKey<String>('home_page'),
+      appBar: AppBar(
+        title: const Text('Video player example'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _BumbleBeeRemoteVideo(
+                url:
+                    'https://videojs-vr.netlify.app/samples/eagle-360.mp4',
+                mediaFormat: MediaFormat.VR2D360),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late Video360Controller controller;
+class _BumbleBeeRemoteVideo extends StatefulWidget {
+  final String url;
+  final int mediaFormat;
 
-  num duration = 0;
-  num total = 0;
-  NumberFormat formatter = new NumberFormat("00");
+  _BumbleBeeRemoteVideo({
+    Key? key,
+    required this.url,
+    required this.mediaFormat,
+  }) : super(key: key);
 
+  @override
+  _BumbleBeeRemoteVideoState createState() => _BumbleBeeRemoteVideoState();
+}
+
+double _cameraPitch = 0;
+double _cameraYaw = 0;
+Vector3 _magnetometer = Vector3.zero();
+Vector3 _accelerometer = Vector3.zero();
+Vector3 _absoluteOrientation = Vector3.zero();
+int interval = Duration.microsecondsPerSecond ~/ 60;
+
+class _BumbleBeeRemoteVideoState extends State<_BumbleBeeRemoteVideo> {
+  late VideoPlayerController _controller;
+  late final StreamSubscription _streamSubscription;
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.network(
+      widget.url,
+      videoPlayerOptions: VideoPlayerOptions(
+        mixWithOthers: true,
+        mediaFormat: widget.mediaFormat,
+      ),
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize();
+
+    motionSensors.accelerometerUpdateInterval = interval;
+    motionSensors.magnetometerUpdateInterval = interval;
+
+    motionSensors.accelerometer.listen((AccelerometerEvent event) {
+      setState(() {
+        _accelerometer.setValues(event.x, event.y, event.z);
+      });
+    });
+
+    motionSensors.magnetometer.listen((MagnetometerEvent event) {
+      setState(() {
+        _magnetometer.setValues(event.x, event.y, event.z);
+        var matrix =
+            motionSensors.getRotationMatrix(_accelerometer, _magnetometer);
+        _absoluteOrientation.setFrom(motionSensors.getOrientation(matrix));
+        double x = degrees(_absoluteOrientation[0]),
+            y = degrees(_absoluteOrientation[1]),
+            z = degrees(_absoluteOrientation[2]);
+        _controller.setCameraRotation(0, 0, x);
+        if (kDebugMode) {
+          print("x=$x y=$y z=$z \r\n");
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var statusBar = MediaQuery.of(context).padding.top;
-
-    var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Video 360 Plugin example app'),
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: Container(
-              width: width,
-              height: height,
-              child: Video360View(
-                onVideo360ViewCreated: _onVideo360ViewCreated,
-                url:
-                    'https://videojs-vr.netlify.app/samples/eagle-360.mp4',
-                onPlayInfo: (Video360PlayInfo info) {
-                  setState(() {
-                    duration = info.duration;
-                    total = info.total;
-                  });
-                },
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(padding: const EdgeInsets.only(top: 20.0)),
+          const Text('With remote mp4'),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  _ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ],
               ),
             ),
           ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  MaterialButton(
-                    onPressed: () {
-                      controller.play();
-                    },
-                    color: Colors.grey[100],
-                    child: Text('Play'),
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      controller.stop();
-                    },
-                    color: Colors.grey[100],
-                    child: Text('Stop'),
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      controller.reset();
-                    },
-                    color: Colors.grey[100],
-                    child: Text('Reset'),
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      controller.jumpTo(80000);
-                    },
-                    color: Colors.grey[100],
-                    child: Text('1:20'),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  MaterialButton(
-                    onPressed: () {
-                      controller.seekTo(-2000);
-                    },
-                    color: Colors.grey[100],
-                    child: Text('<<'),
-                  ),
-                  MaterialButton(
-                    onPressed: () {
-                      controller.seekTo(2000);
-                    },
-                    color: Colors.grey[100],
-                    child: Text('>>'),
-                  ),
-                  Flexible(
-                    child: MaterialButton(
-                      onPressed: () {},
-                      color: Colors.grey[100],
-                      child: Text(
-                          '${formatter.format(duration/1000/60)}:${formatter.format(duration/1000%60)}/ ${formatter.format(total/1000/60)}:${formatter.format(total/1000%60)}'),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          )
         ],
       ),
     );
   }
+}
 
-  _onVideo360ViewCreated(Video360Controller controller) {
-    this.controller = controller;
+class _ControlsOverlay extends StatelessWidget {
+  const _ControlsOverlay({Key? key, required this.controller})
+      : super(key: key);
+
+  static const _examplePlaybackRates = [
+    0.25,
+    0.5,
+    1.0,
+    1.5,
+    2.0,
+    3.0,
+    5.0,
+    10.0,
+  ];
+
+  static const _mediaFormats = {
+    MediaFormat.STANDARD: "Standard",
+    MediaFormat.VR2D180: "Monoscopic 180",
+    MediaFormat.VR2D360: "Monoscopic 360",
+    MediaFormat.VR3D180_OU: "Stereoscopic 180 OverUnder",
+    MediaFormat.VR3D180_SBS: "Stereoscopic 180 SideBySide",
+    MediaFormat.VR3D360_OU: "Stereoscopic 360 OverUnder",
+    MediaFormat.VR3D360_SBS: "Stereoscopic 360 SideBySide",
+  };
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: Duration(milliseconds: 50),
+          reverseDuration: Duration(milliseconds: 200),
+          child: controller.value.isPlaying
+              ? SizedBox.shrink()
+              : Container(
+                  color: Colors.black26,
+                  child: Center(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 100.0,
+                      semanticLabel: 'Play',
+                    ),
+                  ),
+                ),
+        ),
+        GestureDetector(
+          onTap: () {
+            controller.value.isPlaying ? controller.pause() : controller.play();
+          },
+          onPanUpdate: (details) {
+            double touchX = details.delta.dx;
+            double touchY = details.delta.dy;
+            double r = 0;
+            double cr = cos(r);
+            double sr = sin(r);
+            _cameraYaw -= cr * touchX - sr * touchY;
+            _cameraPitch += sr * touchX + cr * touchY;
+            _cameraPitch = max(-45, min(45, _cameraPitch));
+            controller.setCameraRotation(0.0, _cameraPitch, _cameraYaw);
+          },
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: PopupMenuButton<double>(
+            initialValue: controller.value.playbackSpeed,
+            tooltip: 'Playback speed',
+            onSelected: (speed) {
+              controller.setPlaybackSpeed(speed);
+            },
+            itemBuilder: (context) {
+              return [
+                for (final speed in _examplePlaybackRates)
+                  PopupMenuItem(
+                    value: speed,
+                    child: Text('${speed}x'),
+                  )
+              ];
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                // Using less vertical padding as the text is also longer
+                // horizontally, so it feels like it would need more spacing
+                // horizontally (matching the aspect ratio of the video).
+                vertical: 12,
+                horizontal: 16,
+              ),
+              child: Text('${controller.value.playbackSpeed}x'),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: PopupMenuButton<int>(
+            initialValue: controller.value.mediaFormat,
+            tooltip: 'Switch between media format',
+            onSelected: (format) {
+              controller.setMediaFormat(format);
+            },
+            itemBuilder: (context) {
+              return [
+                for (var keyvalue in _mediaFormats.entries)
+                  PopupMenuItem(
+                    value: keyvalue.key,
+                    child: Text(keyvalue.value),
+                  ),
+              ];
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                // Using less vertical padding as the text is also longer
+                // horizontally, so it feels like it would need more spacing
+                // horizontally (matching the aspect ratio of the video).
+                vertical: 12,
+                horizontal: 16,
+              ),
+              child: Text(_mediaFormats[controller.value.mediaFormat]!),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
