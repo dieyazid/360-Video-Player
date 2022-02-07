2fcbf45de56a91a355723ab71457a832b369f726
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2022-02-07 23:35:40 +0100

    pUSH

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,146 +1,193 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
-/// An example of using the plugin, controlling lifecycle and playback of the
-/// video.
-
-import 'dart:async';
-import 'dart:math';
-import 'dart:ui';
-import 'lowpass.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player_extra/video_player_extra.dart';
-import 'package:motion_sensors/motion_sensors.dart';
-import 'package:vector_math/vector_math_64.dart' hide Colors;
+import 'package:google_fonts/google_fonts.dart';
 
+const c_green = Color(0xFF45cbb8);
 void main() {
-  runApp(
-    MaterialApp(
-      home: _App(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class _App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '360 Video Player',
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePages createState() => HomePages();
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SearchSection(),
+              VideoSection(),
+            ],
+          ),
+        ),
+      );
+}
+
+class Cloud extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Container());
+}
+
+class Local extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Container());
+}
+
+class HomePages extends State<HomePage> {
+  @override
+  int index = 0;
+  final pages = [Home(), Cloud(), Local()];
   Widget build(BuildContext context) {
     return Scaffold(
-      key: const ValueKey<String>('home_page'),
-      appBar: AppBar(
-        title: const Text('Video player  example'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            _BumbleBeeRemoteVideo(
-                url:
-                    'https://github.com/medbenzekri/360_samples/blob/main/Lions%20360%C2%B0%20_%20National%20Geographic%20%5BsPyAQQklc1s%5D.mp4?raw=true',
-                mediaFormat: MediaFormat.VR2D360),
-          ],
-        ),
-      ),
-    );
+        appBar: MyAppBar(),
+        body: pages[index],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+              indicatorColor: Color(0xFFfff4eb),
+              height: 60,
+              backgroundColor: c_green,
+              labelTextStyle: MaterialStateProperty.all(
+                TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              )),
+          child: NavigationBar(
+            selectedIndex: index,
+            onDestinationSelected: (index) =>
+                setState(() => this.index = index),
+            destinations: [
+              NavigationDestination(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+                label: 'Home',
+                selectedIcon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                ),
+              ),
+              NavigationDestination(
+                icon: Icon(
+                  Icons.cloud,
+                  color: Colors.white,
+                ),
+                label: 'Cloud',
+                selectedIcon: Icon(
+                  Icons.cloud,
+                  color: Colors.black,
+                ),
+              ),
+              NavigationDestination(
+                icon: Icon(
+                  Icons.folder,
+                  color: Colors.white,
+                ),
+                label: 'Local',
+                selectedIcon: Icon(
+                  Icons.folder,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
-class _BumbleBeeRemoteVideo extends StatefulWidget {
-  final String url;
-  final int mediaFormat;
-
-  _BumbleBeeRemoteVideo({
-    Key? key,
-    required this.url,
-    required this.mediaFormat,
-  }) : super(key: key);
-
-  @override
-  _BumbleBeeRemoteVideoState createState() => _BumbleBeeRemoteVideoState();
-}
-
-double _cameraPitch = 0;
-double _cameraYaw = 0;
-Vector3 _magnetometer = Vector3.zero();
-Vector3 _accelerometer = Vector3.zero();
-Vector3 _absoluteOrientation = Vector3.zero();
-int interval = Duration.microsecondsPerSecond ~/ 60;
-double x = 0, y = 0, z = 0, smoothSpeed = 0.125;
-
-class _BumbleBeeRemoteVideoState extends State<_BumbleBeeRemoteVideo> {
-  late VideoPlayerController _controller;
-  late final StreamSubscription _streamSubscription;
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-      widget.url,
-      videoPlayerOptions: VideoPlayerOptions(
-        mixWithOthers: true,
-        mediaFormat: widget.mediaFormat,
-      ),
-    );
-
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(true);
-    _controller.initialize();
-
-    motionSensors.accelerometerUpdateInterval = interval;
-    motionSensors.magnetometerUpdateInterval = interval;
-
-    motionSensors.accelerometer.listen((AccelerometerEvent event) {
-      setState(
-          () => _accelerometer = applyLowPassFilter(event, _accelerometer));
-    });
-
-    motionSensors.magnetometer.listen((MagnetometerEvent event) {
-      setState(() {
-        _magnetometer = applyLowPassFilter(event, _magnetometer);
-        var matrix =
-            motionSensors.getRotationMatrix(_accelerometer, _magnetometer);
-        _absoluteOrientation.setFrom(motionSensors.getOrientation(matrix));
-
-        x = degrees(_absoluteOrientation.x);
-        y = degrees(_absoluteOrientation.y);
-        z = degrees(_absoluteOrientation.z);
-        _controller.setCameraRotation(0, -y + 90, x);
-        if (kDebugMode) {
-          print("x=$x y=$y z=$z \r\n");
-        }
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  Size get preferredSize => new Size.fromHeight(50);
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return AppBar(
+      title: Text(
+        '360VP',
+        style: GoogleFonts.nunito(
+            color: Colors.black, fontSize: 22, fontWeight: FontWeight.w800),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+    );
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: Color(0xFFeaeaea),
+      padding: EdgeInsets.fromLTRB(10, 25, 10, 10),
       child: Column(
-        children: <Widget>[
-          Container(padding: const EdgeInsets.only(top: 20.0)),
-          const Text('With remote mp4'),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  VideoPlayer(_controller),
-                  _ControlsOverlay(controller: _controller),
-                  VideoProgressIndicator(_controller, allowScrubbing: true),
-                ],
+        children: [
+          Row(
+            children: [
+              Expanded(
+                  child: Container(
+                padding: EdgeInsets.only(left: 5),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                        offset: Offset(0, 1),
+                      )
+                    ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Video title',
+                    contentPadding: EdgeInsets.all(10),
+                    border: InputBorder.none,
+                  ),
+                ),
+              )),
+              SizedBox(
+                width: 10,
               ),
-            ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                      )
+                    ],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25),
+                    )),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.search,
+                    size: 26,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.all(10),
+                    primary: c_green,
+                  ),
+                ),
+              )
+            ],
           ),
         ],
       ),
@@ -148,129 +195,12 @@ class _BumbleBeeRemoteVideoState extends State<_BumbleBeeRemoteVideo> {
   }
 }
 
-class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({Key? key, required this.controller})
-      : super(key: key);
-
-  static const _examplePlaybackRates = [
-    0.25,
-    0.5,
-    1.0,
-    1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
-  ];
-
-  static const _mediaFormats = {
-    MediaFormat.STANDARD: "Standard",
-    MediaFormat.VR2D180: "Monoscopic 180",
-    MediaFormat.VR2D360: "Monoscopic 360",
-    MediaFormat.VR3D180_OU: "Stereoscopic 180 OverUnder",
-    MediaFormat.VR3D180_SBS: "Stereoscopic 180 SideBySide",
-    MediaFormat.VR3D360_OU: "Stereoscopic 360 OverUnder",
-    MediaFormat.VR3D360_SBS: "Stereoscopic 360 SideBySide",
-  };
-
-  final VideoPlayerController controller;
-
+class VideoSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AnimatedSwitcher(
-          duration: Duration(milliseconds: 50),
-          reverseDuration: Duration(milliseconds: 200),
-          child: controller.value.isPlaying
-              ? SizedBox.shrink()
-              : Container(
-                  color: Colors.black26,
-                  child: Center(
-                    child: Icon(
-                      Icons.play_arrow,
-                      color: Colors.white,
-                      size: 100.0,
-                      semanticLabel: 'Play',
-                    ),
-                  ),
-                ),
-        ),
-        GestureDetector(
-          onTap: () {
-            controller.value.isPlaying ? controller.pause() : controller.play();
-          },
-          onPanUpdate: (details) {
-            double touchX = details.delta.dx;
-            double touchY = details.delta.dy;
-            double r = 0;
-            double cr = cos(r);
-            double sr = sin(r);
-            _cameraYaw -= cr * touchX - sr * touchY;
-            _cameraPitch += sr * touchX + cr * touchY;
-            _cameraPitch = max(-45, min(45, _cameraPitch));
-            controller.setCameraRotation(0.0, _cameraPitch, _cameraYaw);
-          },
-        ),
-        Align(
-          alignment: Alignment.topRight,
-          child: PopupMenuButton<double>(
-            initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
-            onSelected: (speed) {
-              controller.setPlaybackSpeed(speed);
-            },
-            itemBuilder: (context) {
-              return [
-                for (final speed in _examplePlaybackRates)
-                  PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  )
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text('${controller.value.playbackSpeed}x'),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.topLeft,
-          child: PopupMenuButton<int>(
-            initialValue: controller.value.mediaFormat,
-            tooltip: 'Switch between media format',
-            onSelected: (format) {
-              controller.setMediaFormat(format);
-            },
-            itemBuilder: (context) {
-              return [
-                for (var keyvalue in _mediaFormats.entries)
-                  PopupMenuItem(
-                    value: keyvalue.key,
-                    child: Text(keyvalue.value),
-                  ),
-              ];
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                // Using less vertical padding as the text is also longer
-                // horizontally, so it feels like it would need more spacing
-                // horizontally (matching the aspect ratio of the video).
-                vertical: 12,
-                horizontal: 16,
-              ),
-              child: Text(_mediaFormats[controller.value.mediaFormat]!),
-            ),
-          ),
-        ),
-      ],
+    return Container(
+      height: 2000,
+      color: Colors.white,
     );
   }
 }
